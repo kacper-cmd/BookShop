e9cd43efe39e5db80fa883e9007021752f61f3ff
kacper-cmd <kacperobrzut63@gmail.com>
2023-03-23 17:39:15 +0100

    first commit

---
+++ BookShop.Intranet/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Intranet.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
